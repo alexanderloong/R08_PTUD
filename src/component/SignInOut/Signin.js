210254c7e00647470f900b833633e256146f73f3
Alexander Loong <dj.thaomeo@gmail.com>
2022-01-08 18:32:27 +0700

    Update signin

--- src/component/SignInOut/Signin.js
+++ src/component/SignInOut/Signin.js
@@ -5,55 +5,80 @@ const Signin = () => {
     <section className="ftco-section">
       <div className="container">
         <div className="row justify-content-center">
-          <div className="col-md-6 text-center mb-5">
-            <h2 className="heading-section">Login #08</h2>
-          </div>
-        </div>
-        <div className="row justify-content-center">
-          <div className="col-md-6 col-lg-5">
-            <div className="login-wrap p-4 p-md-5">
-              <div className="icon d-flex align-items-center justify-content-center">
-                <span className="fa fa-user-o"></span>
-              </div>
-              <h3 className="text-center mb-4">Have an account?</h3>
-              <form action="#" className="login-form">
-                <div className="form-group">
-                  <input
-                    type="text"
-                    className="form-control rounded-left"
-                    placeholder="Username"
-                    required
-                  />
+          <div className="col-md-7 col-lg-5">
+            <div className="wrap">
+              <div className="login-wrap p-4 p-md-5">
+                <div className="d-flex">
+                  <div className="w-100">
+                    <h3 className="mb-4">Đăng Nhập</h3>
+                  </div>
+                  <div className="w-100">
+                    <p className="social-media d-flex justify-content-end">
+                      <a
+                        href="/"
+                        className="social-icon d-flex align-items-center justify-content-center"
+                      >
+                        <span className="fa fa-facebook"></span>
+                      </a>
+                      <a
+                        href="/"
+                        className="social-icon d-flex align-items-center justify-content-center"
+                      >
+                        <span className="fa fa-twitter"></span>
+                      </a>
+                    </p>
+                  </div>
                 </div>
-                <div className="form-group d-flex">
-                  <input
-                    type="password"
-                    className="form-control rounded-left"
-                    placeholder="Password"
-                    required
-                  />
-                </div>
-                <div className="form-group d-md-flex">
-                  <div className="w-50">
-                    <label className="checkbox-wrap checkbox-primary">
-                      Remember Me
-                      <input type="checkbox" checked />
-                      <span className="checkmark"></span>
+                <form action="#" className="signin-form">
+                  <div className="form-group mt-3">
+                    <input type="text" className="form-control" required />
+                    <label className="form-control-placeholder" type="username">
+                      Số điện thoại/Email
                     </label>
                   </div>
-                  <div className="w-50 text-md-right">
-                    <a href="/">Forgot Password</a>
+                  <div className="form-group">
+                    <input
+                      id="password-field"
+                      type="password"
+                      className="form-control"
+                      required
+                    />
+                    <label className="form-control-placeholder" type="password">
+                      Mật khẩu
+                    </label>
+                    <span
+                      toggle="#password-field"
+                      className="fa fa-fw fa-eye field-icon toggle-password"
+                    ></span>
                   </div>
-                </div>
-                <div className="form-group">
-                  <button
-                    type="submit"
-                    className="btn btn-primary rounded submit p-3 px-5"
-                  >
-                    Get Started
-                  </button>
-                </div>
-              </form>
+                  <div className="form-group">
+                    <button
+                      type="submit"
+                      className="form-control btn btn-primary rounded submit px-3"
+                    >
+                      Đăng nhập
+                    </button>
+                  </div>
+                  <div className="form-group d-md-flex">
+                    <div className="w-50 text-left">
+                      <label className="checkbox-wrap checkbox-primary mb-0">
+                        Nhớ mật khẩu
+                        <input type="checkbox" defaultChecked />
+                        <span className="checkmark"></span>
+                      </label>
+                    </div>
+                    <div className="w-50 text-md-right">
+                      <a href="/">Quên mật khẩu</a>
+                    </div>
+                  </div>
+                </form>
+                <p className="text-center">
+                  Bạn chưa có tài khoản?{" "}
+                  <a data-toggle="tab" href="/signup">
+                    Đăng ký
+                  </a>
+                </p>
+              </div>
             </div>
           </div>
         </div>
